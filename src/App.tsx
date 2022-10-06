4e586e5b8bc8a7159ec5defc8530d88d05ef35b1
Logan Walker <walker@sionic.io>
2022-10-05 21:18:54 -0400

    legal move logic but its glitchy

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,65 @@ import { Chess } from "chess.js";
 import "chessground/assets/chessground.base.css";
 import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
+import { move } from "chessground/drag";
 
 function App() {
     const chess = new Chess();
 
     const [fenState, setFen] = useState(chess.fen());
 
+    const [legal_moves, set_legal_moves] = useState<any>();
+
     const handleMove = (orig: string, dest: string, capturedPiece: any) => {
         chess.move(dest);
-        console.log(chess.board());
+        const fen = chess.fen();
+        setFen(fen);
     };
 
-    const getEval = (fen: string) => {};
+    const findLegalMoves = (sq: any) => {
+        const allMoves = chess.moves();
+        console.log(allMoves);
+        const verboseMoves: Array<any> = chess.moves({
+            square: sq,
+            verbose: true,
+        });
+        console.log(verboseMoves);
+        const moves: Array<string> = [];
+        verboseMoves.forEach((move) => moves.push(move.to));
+        const moveObj = new Map();
+        moveObj.set(sq, moves);
+        console.log(moveObj);
+        set_legal_moves(moveObj);
+    };
 
     return (
         <div className="App">
             <div className="chess-container">
                 <Chessground
                     config={{
+                        fen: fenState,
+                        movable: {
+                            free: false,
+                            dests: legal_moves,
+                            showDests: true,
+                            events: {
+                                after: (
+                                    orig: string,
+                                    dest: string,
+                                    capturedPiece: any
+                                ) => {
+                                    handleMove(orig, dest, capturedPiece);
+                                },
+                            },
+                        },
+                        selectable: {
+                            enabled: false,
+                        },
                         events: {
-                            move: (
-                                orig: string,
-                                dest: string,
-                                capturedPiece: any
-                            ) => {
-                                handleMove(orig, dest, capturedPiece);
+                            select: (key: any) => {
+                                if (chess.moves({ square: key }).length) {
+                                    findLegalMoves(key);
+                                }
                             },
                         },
                     }}
