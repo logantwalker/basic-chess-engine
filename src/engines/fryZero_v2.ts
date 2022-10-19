f442b34e01d145fc716cb556a5268224d9e450c9
Logan Walker <walker@sionic.io>
2022-10-19 15:59:07 -0400

    a new paradigm (*echoes* paradigm paradigm para..)

--- src/engines/fryZero_v2.ts
+++ src/engines/fryZero_v2.ts
@@ -19,6 +19,9 @@ declare type Key = "a0" | `${File}${Rank}`
  ===============================================
 \************************************************/
 
-const fryZero_v2 = (gameState: Chess) => {}
+// after doing a ton of frikin research about this topic, im going to try and implement this whole thing from scratch without chess.js.
+// i think avoiding the black box of chess.js is probably going to lead to better result, and a better understanding of what exactly is going on in the brain of the engine.
+
+const fryZero_v2 = (idk_yet: any) => {}
 
 export default fryZero_v2
